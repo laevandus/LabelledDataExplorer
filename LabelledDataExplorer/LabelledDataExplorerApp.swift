607e69da466ab4285304421485e4d71fbb1ca104
Toomas Vahter <toomas.vahter@gmail.com>
2023-11-23 09:43:55 +0200

    Initial Commit

---
+++ LabelledDataExplorer/LabelledDataExplorerApp.swift
@@ -0,0 +1,17 @@
+//
+//  LabelledDataExplorerApp.swift
+//  LabelledDataExplorer
+//
+//  Created by Toomas Vahter on 23.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct LabelledDataExplorerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
